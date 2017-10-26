8b9942db714c859522ef5554704eca850059463f
G-gonn <skander.apollo@gmail.com>
2017-10-26 16:10:27 +0200

    Initial commit

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,18 @@
+class CoursesController < ApplicationController
+
+	def index
+	end
+
+	def ror
+	end
+
+	def ruby
+	end
+
+	def htmlcss
+	end
+
+	def javascript
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+	root "courses#index"
+
+	get "/ruby", to: "courses#ruby"
+
+	get "/ror", to: "courses#ror"
+
+	get "/htmlcss", to: "courses#htmlcss"
+
+	get "/javascript", to: "courses#javascript"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
